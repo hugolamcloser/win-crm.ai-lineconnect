2c685e04e5f35d83d371c7dfdbc8b7f5602549f7
hugolamcloser <hugolamcloser@gmail.com>
2026-07-02 18:43:30 +0800

    Align LINE-GHL middleware use case

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { logger } from "./config/logger";
 import { jsonBodyParser } from "./middleware/jsonBody";
 import { errorHandler, notFoundHandler } from "./middleware/errors";
 import { adminRouter } from "./routes/admin";
+import { debugRouter } from "./routes/debug";
 import { ghlWebhookRouter } from "./routes/ghlWebhook";
 import { healthRouter } from "./routes/health";
 import { lineWebhookRouter } from "./routes/lineWebhook";
@@ -18,6 +19,7 @@ export function createApp() {
   app.use(jsonBodyParser);
 
   app.use(healthRouter);
+  app.use(debugRouter);
   app.use(lineWebhookRouter);
   app.use(ghlWebhookRouter);
   app.use(adminRouter);
